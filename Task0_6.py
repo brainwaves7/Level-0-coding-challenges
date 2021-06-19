d4973b6a19cb0917ee15ddc48304d9232de2cf66
brainwaves7 <85312173+brainwaves7@users.noreply.github.com>
2021-06-19 16:32:25 +0200

    Task 0.6

---
+++ Task0_6.py
@@ -0,0 +1,32 @@
+#Task 0.6 (A function that determines the maximum number)
+print("Task 0.6")
+
+def maximum(a, b, c):
+    highest = 0
+    if a > b and a > c :
+        highest = a
+    elif b > a and b > c:
+        highest = b
+    else:
+        highest = c
+    return highest
+
+print(maximum(9, 15, 3))
+
+#Bonus addition
+
+def maximum(a, b, c, d):
+    highest = 0
+    if a > b and a > c :
+        highest = a
+    elif b > a and b > c:
+        highest = b
+    else:
+        highest = c
+    
+    if highest < d:
+        highest = d
+    
+    return highest
+
+print(maximum(1, 22, 3, 2))

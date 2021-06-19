18be54700afabd449775e1493578b7293e918538
brainwaves7 <85312173+brainwaves7@users.noreply.github.com>
2021-06-19 16:39:14 +0200

    Task 0.7

---
+++ Task0_7.py
@@ -0,0 +1,15 @@
+#Task 0.7 (Temperature conversion functions)
+print("Task 0.7")
+
+degrees = 0
+def celsius_to_fahrenheit(deg) :
+    degrees = (deg * 9 / 5) + 32
+    return degrees
+
+print(celsius_to_fahrenheit(25)) 
+
+def fahrenheit_to_celsius(num) :
+    degrees = (num - 32) / 1.8
+    return degrees
+
+print(fahrenheit_to_celsius(77))

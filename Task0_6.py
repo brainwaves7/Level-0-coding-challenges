3465d67d55f1f02bcc183889335916494af5550c
brainwaves7 <85312173+brainwaves7@users.noreply.github.com>
2021-07-13 22:04:52 +0200

    Task 0.6

--- Task0_6.py
+++ Task0_6.py
@@ -1,12 +1,13 @@
-#Task 0.6
-def maximum(*a):
-    large = a[0]
-    for element in a:
-        if element >= large:
-            large = element
-    return large
+def maximum(*numbers):
+    largest_number = numbers[0]
+    for element in numbers:
+        if element >= largest_number:
+            largest_number = element
+    return largest_number
+
 
 print(maximum(9, 15, 3))
 
-#Bonus addition works with orginal function
+# Bonus addition works with orginal function
+
 print(maximum(1, 22, 3, 2))

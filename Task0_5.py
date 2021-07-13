6e2be81f968b744ecb2d191cb142084da6fe6708
brainwaves7 <85312173+brainwaves7@users.noreply.github.com>
2021-07-13 21:59:02 +0200

    Task 0.5

--- Task0_5.py
+++ Task0_5.py
@@ -1,9 +1,17 @@
-#Task 0.5
 import math
 
-def area_of_a_triangle(a, b, c):
-    parameter = (a + b + c) / 2
-    area = math.sqrt((parameter * (parameter - a) * (parameter - b) * (parameter - c)))
+
+def area_of_a_triangle(side_a, side_b, side_c):
+    parameter_of_triangle = (side_a + side_b + side_c) / 2
+    area = math.sqrt(
+        (
+            parameter_of_triangle
+            * (parameter_of_triangle - side_a)
+            * (parameter_of_triangle - side_b)
+            * (parameter_of_triangle - side_c)
+        )
+    )
     return area
+
 
 print(area_of_a_triangle(5, 3, 4))

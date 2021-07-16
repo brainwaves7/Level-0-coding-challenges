2f986897705f282df8fc3a3ab30f27487205c8cf
brainwaves7 <85312173+brainwaves7@users.noreply.github.com>
2021-07-16 11:45:25 +0200

    Task 0.6 no print update

--- Task0_6.py
+++ Task0_6.py
@@ -4,8 +4,3 @@ def maximum(*numbers):
         if element >= largest_number:
             largest_number = element
     return largest_number
-
-
-if __name__ == "__main__":
-    print(maximum(9, 15, 3))
-    print(maximum(1, 22, 3, 2))

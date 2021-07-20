c5e64aa7133e77e97605fd2c536613d29ccc08ae
brainwaves7 <85312173+brainwaves7@users.noreply.github.com>
2021-07-21 00:52:47 +0200

    Added main to Task 0.5

--- Task0_5.py
+++ Task0_5.py
@@ -12,3 +12,11 @@ def area_of_a_triangle(side_a, side_b, side_c):
         )
     )
     return area
+
+
+def main():
+    print(area_of_a_triangle(3, 4, 5))
+
+
+if __name__ == "__main__":
+    main()

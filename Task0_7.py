b4fc54f202ce02897ed9f9078df68854f7615ffb
brainwaves7 <85312173+brainwaves7@users.noreply.github.com>
2021-07-21 01:02:03 +0200

    Added main to Task 0.7

--- Task0_7.py
+++ Task0_7.py
@@ -6,3 +6,12 @@ def celsius_to_fahrenheit(degrees_to_convert):
 def fahrenheit_to_celsius(degrees_to_convert):
     temperature = (degrees_to_convert - 32) / 1.8
     return temperature
+
+
+def main():
+    print(celsius_to_fahrenheit(25))
+    print(fahrenheit_to_celsius(77))
+
+
+if __name__ == "__main__":
+    main()

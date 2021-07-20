a46710b8c610c4887f71bd4926d95998d4eb6314
brainwaves7 <85312173+brainwaves7@users.noreply.github.com>
2021-07-21 01:06:16 +0200

    Omit while loop update for Task 0.9

--- Task0_9.py
+++ Task0_9.py
@@ -5,10 +5,17 @@ def vowels_in_string(a_string):
     a_string = a_string.casefold()
 
     for element in a_string:
-        if element in vowels:
-            while element not in result:
-                result.append(element)
+        if element in vowels and element not in result:
+            result.append(element)
 
     result = ", ".join(result)
 
     print(f"Vowels: {result}")
+
+
+def main():
+    vowels_in_string("Umuzi")
+
+
+if __name__ == "__main__":
+    main()
